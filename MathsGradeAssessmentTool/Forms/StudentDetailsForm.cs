1b6f7d358b801edb6ed1b394955ef292d34c455b
Somshubra Majumdar <titu1994@gmail.com>
2015-08-04 17:54:41 +0530

    Added Code to Select and Export selected students into excel file

--- MathsGradeAssessmentTool/Forms/StudentDetailsForm.cs
+++ MathsGradeAssessmentTool/Forms/StudentDetailsForm.cs
@@ -23,7 +23,6 @@ namespace MathsGradeAssessmentTool.Forms
             this.Validate();
             this.studentBindingSource.EndEdit();
             this.tableAdapterManager.UpdateAll(this.mathsToolDatabaseDataSet);
-
         }
 
         private void StudentDetailsForm_Load(object sender, EventArgs e)
@@ -55,7 +54,7 @@ namespace MathsGradeAssessmentTool.Forms
             this.tableAdapterManager.UpdateAll(this.mathsToolDatabaseDataSet);
 
         }
-        
+
 
         private void selectStudentAndSeeCompetencies_Click(object sender, EventArgs e)
         {
@@ -70,7 +69,101 @@ namespace MathsGradeAssessmentTool.Forms
                 this.Hide();
             }
             catch (Exception ex) { }
-           
+
+        }
+
+        private void SaveSelectedStudentDetails_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (2003)|*.xls";
+            sfd.FileName = "All Student Competencies.xls";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                ToeXcel(studentCompetencyDataAdapter, sfd.FileName);
+            }
+        }
+
+        private void ToeXcel(DataGridView dGV, string filename)
+        {
+
+            string stOutput = "";
+
+            // Export titles:
+            string sHeaders = "";
+
+            for (int j = 0; j < dGV.Columns.Count; j++)
+            {
+                if (j != 0)
+                    sHeaders = sHeaders.ToString() + Convert.ToString(dGV.Columns[j].HeaderText) + "\t";
+                else
+                {
+                    sHeaders = sHeaders.ToString() + "Competency Name" + "\t" + Convert.ToString(dGV.Columns[j].HeaderText) + "\t";
+                }
+            }
+
+            stOutput += sHeaders + "\r\n";
+
+            int selectedRowCount = studentDataGridView.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+            for (int k = 0; k < selectedRowCount; k++)
+            {
+                Console.WriteLine("K : " + k);
+                int studentid = 0;
+                try
+                {
+                    studentid = Convert.ToInt32(studentDataGridView.SelectedRows[k].Cells[0].Value);
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Student Id : " + studentid);
+                studentCompentencyTableAdapter1.FillByStudentID(mathsToolDatabaseDataSet.StudentCompentency, studentid);
+                Console.WriteLine("Table Adapter Row Count : " + studentCompetencyDataAdapter.RowCount);
+
+                for (int i = 0; i < dGV.RowCount; i++)
+                {
+                    string stLine = "";
+                    for (int j = 0; j < dGV.Rows[i].Cells.Count; j++)
+                    {
+                        if (j != 0)
+                        {
+                            stLine = stLine.ToString() + Convert.ToString(dGV.Rows[i].Cells[j].Value) + "\t";
+                        }
+                        else
+                        {
+                            String competencyName = "";
+                            try
+                            {
+                                var row = competencyTableAdapter1.GetCompetencyNameByID(Convert.ToInt32(dGV.Rows[i].Cells[j].Value))[0];
+                                competencyName = row.CompetencyName;
+                            }
+                            catch (Exception ex)
+                            {
+                                competencyName = "Competency " + Convert.ToString(dGV.Rows[i].Cells[j].Value);
+                            }
+
+                            stLine = stLine.ToString() + competencyName + "\t" + Convert.ToString(dGV.Rows[i].Cells[j].Value) + "\t";
+                        }
+                    }
+
+                    stOutput += stLine + "\r\n";
+                }
+
+            }
+
+            Encoding utf16 = Encoding.UTF8;
+            byte[] output = utf16.GetBytes(stOutput);
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(output, 0, output.Length); //write the encoded file
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+
+            var fileInfo = new FileInfo(filename) { IsReadOnly = true };
         }
     }
 }
